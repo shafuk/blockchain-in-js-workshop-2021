f32df85cb13a3288ddc3bcb88e4eb658c7553359
shafuk <2424834335@qq.com>
2023-06-01 12:11:27 +0800

    update

--- src/models/Block.js
+++ src/models/Block.js
@@ -5,11 +5,27 @@ export const DIFFICULTY = 2
 class Block {
   // 1. 完成构造函数及其参数
 
-  constructor() {}
+  constructor(chain,previousHash,index,hash) {
+    this.chain=chain
+    this.previousHash=previousHash
+    this.index=index
+    this.hash=hash
+    this.nonce=0
+  }
 
-  isValid() {}
+  isValid(){
+    var temp = this.nonce.toString()
+    var str = "00"
+    if (temp.substring(0,DIFFICULTY)==str){
+      return true
+    }else {
+      return false
+    }
+  }
 
-  setNonce(nonce) {}
+  setNonce(nonce) {
+    this.nonce=nonce
+  }
 }
 
 export default Block
--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -8,14 +8,40 @@ class Blockchain {
       - 创世区块
       - 存储区块的映射
   */
-  constructor() {}
+  constructor(chainname) {
+    this.chainname=chainname
+    this.genesis=null
+    this.blocks=new Map()
+  }
 
   // 2. 定义 longestChain 函数
   /*
     返回当前链中最长的区块信息列表
   */
   longestChain() {
-    return []
+    let value_list = []
+    for (let [key,value] of this.blocks) {
+      value_list.push(value)
+    }
+    let temp = []
+    for (let i = 0; i < value_list.length; i++) {
+      for (let j = 0; j < value_list.length; j++) {
+        if (value_list[j].previousHash==value_list[i].hash) {
+          temp.push([value_list[i],value_list[j]])
+        }
+      }
+    }
+    for (let i = 0; i < temp.length; i++) {
+      for (let j = 0; j < temp.length; j++) {
+        if (temp[i][1]==temp[j][0]  ) {
+          let temp1 = temp[i]
+          let temp2 =temp[j]
+          temp1.pop()
+          return temp1.concat(temp2)
+        }
+      }
+    }
+    return temp[1]
   }
 
   // 判断当前区块链是否包含
