a6fccdc9192c0fc737883b420fa323e7eadd6bfa
shafuk <2424834335@qq.com>
2023-06-07 19:43:05 +0800

    update

--- src/models/Block.js
+++ src/models/Block.js
@@ -26,53 +26,9 @@ class Block {
       this.hash === this._calculateHash())
   }
 
-  createChild(coinbaseBeneficiary) {
-    return new Block({
-      blockchain: this.blockchain,
-      parentHash: this.hash,
-      height: this.height + 1,
-      coinbaseBeneficiary
-    })
-  }
   _calculateHash() {
     return sha256(this.nonce + this.parentHash + this.coinbaseBeneficiary).toString()
   }
-  _setHash() {
-    this.hash = sha256(this.nonce + this.parentHash).toString()
-  }
-
-  // 汇总计算交易的 Hash 值
-  /**
-   * 默克尔树实现
-   */
-  merkleTree(transactions) {
-    if (transactions.length === 1) {
-      return transactions[0];
-    }
-    const newTransactions = [];
-    for (let i = 0; i < transactions.length; i += 2) {
-      const left = transactions[i];
-      const right = i + 1 === transactions.length ? left : transactions[i + 1];
-      const combinedHash = hash(left + right);
-      newTransactions.push(combinedHash);
-    }
-    return merkleTree(newTransactions);
-  }
-  
-  combinedTransactionsHash(transactions) {
-    const merkleRoot = merkleTree(transactions);
-    return hash(merkleRoot);
-  }
-  
-  addTransaction(inputPublicKey, outputPublicKey, amount, fee) {
-    if (!this.isValidTransaction(inputPublicKey, amount, fee))
-      return
-    const transaction = new Transaction(inputPublicKey, outputPublicKey, amount, fee)
-    this.transactions[transaction.hash] = transaction
-    this.utxoPool.handleTransaction(transaction, this.coinbaseBeneficiary)
-    this._setHash();
-  }
-
 
 }
 
